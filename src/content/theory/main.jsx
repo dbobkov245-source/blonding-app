a9e31173ddf49ddfaa4aabd86f9157951a87e7cc
dbobkov245-source <dbobkov245@gmail.com>
2025-11-03 17:30:36 +0300

    Update main.jsx

--- src/content/theory/main.jsx
+++ src/content/theory/main.jsx
@@ -1,12 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 
-function App() {
-  return <h1>Привет, Blonding App работает!</h1>;
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
